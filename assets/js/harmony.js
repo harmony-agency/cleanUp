98e3831bd1c5f4ae8f09ee647d7f6aec99650545
Vahideh-Asemi <v.as74@yahoo.com>
2022-08-10 17:19:24 +0430

    commit v1

--- assets/js/harmony.js
+++ assets/js/harmony.js
@@ -1,9 +1,9 @@
 //  Slide carousels
 var slideswiper = new Swiper(".slideSwiper", {
-  // autoplay: {
-  //   delay: 6000,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 6000,
+    disableOnInteraction: false,
+  },
   navigation: {
     nextEl: ".slideSwiper .swiper-button-next",
     prevEl: ".slideSwiper .swiper-button-prev",
@@ -13,16 +13,14 @@ var slideswiper = new Swiper(".slideSwiper", {
   },
 });
 
-
-
 //  banner carousels
 var bannerSwiper = new Swiper(".bannerSwiper", {
   slidesPerView: 1.5,
   centeredSlides: true,
   loop: true,
-  // autoplay: {
-  //   delay: 6000,
-  // },
+  autoplay: {
+    delay: 6000,
+  },
   navigation: {
     nextEl: ".bannerSwiper .swiper-button-next",
     prevEl: ".bannerSwiper .swiper-button-prev",
@@ -40,8 +38,60 @@ var bannerSwiper = new Swiper(".bannerSwiper", {
   //     spaceBetween: 40,
   //   },
   //   1024: {
-  //     slidesPerView: 3,
+  //     slidesPerView: 1.5,
   //     spaceBetween: 50,
   //   },
-  // },
+});
+
+// hover for copy right
+$(".copyright a").hover(
+  function () {
+    let logo = $(".copyright .hover-company");
+    logo.addClass("active");
+  },
+  function () {
+    let logo = $(".copyright .hover-company");
+    logo.removeClass("active");
+  }
+);
+
+// stickyHeader
+jQuery(function ($) {
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 100 && $(this).scrollTop() < 4300) {
+      $("header").addClass("stickyHeader");
+    } else {
+      $("header").removeClass("stickyHeader");
+    }
+  });
+});
+
+//  box carousels
+var boxSwiper = new Swiper(".boxSwiper", {
+  // centeredSlides: true,
+  // loop: true,
+  autoplay: {
+    delay: 6000,
+  },
+  navigation: {
+    nextEl: ".bannerSwiper .swiper-button-next",
+    prevEl: ".bannerSwiper .swiper-button-prev",
+  },
+  pagination: {
+    el: ".bannerSwiper .swiper-pagination",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
 });
